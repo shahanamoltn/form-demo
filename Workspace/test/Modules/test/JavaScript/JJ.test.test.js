cc96999491196a44632eb2cf5d8dab10dfccfe33
shahanamoltn <shahana.nazar@jobinandjismi.com>
2026-04-29 15:33:10 +0530

    cart fix

--- Workspace/test/Modules/test/JavaScript/JJ.test.test.js
+++ Workspace/test/Modules/test/JavaScript/JJ.test.test.js
@@ -1,15 +1,18 @@
 define(
     'JJ.test.test',
     [
-        'JJ.test.test.View'
+        'JJ.test.test.View',
+        'JJ.test.test.Cart.View'
     ],
     function (
-        TestView
+        TestView,
+        TestCartView
     ) {
         'use strict';
-
         return {
             mountToApp: function mountToApp(container) {
+                console.log('--- TEST EXTENSION LOADED ---');
+                
                 var pdp = container.getComponent('PDP');
                 if (pdp) {
                     pdp.addChildViews(pdp.PDP_FULL_VIEW, {
@@ -23,8 +26,60 @@ define(
                         }
                     });
                 }
+
+                // Try to get CartLinesView safely
+                var CartLinesView;
+                try {
+                    CartLinesView = require('Cart.Lines.View');
+                } catch (e) {
+                    console.log('Cart.Lines.View module not found, using Component API instead.');
+                }
+
+                if (CartLinesView) {
+                    console.log('--- ATTACHING VIA PROTOTYPE ---');
+                    CartLinesView.addChildViews({
+                        'Item.Summary': {
+                            'JJ.test.test.Cart.View.Summary': {
+                                childViewIndex: 10,
+                                childViewConstructor: function () {
+                                    return new TestCartView({ model: this.model });
+                                }
+                            }
+                        },
+                        'Item.Sku': {
+                            'JJ.test.test.Cart.View.Sku': {
+                                childViewIndex: 11,
+                                childViewConstructor: function () {
+                                    return new TestCartView({ model: this.model });
+                                }
+                            }
+                        }
+                    });
+                } else {
+                    var cart = container.getComponent('Cart');
+                    if (cart) {
+                        console.log('--- ATTACHING VIA COMPONENT ---');
+                        cart.addChildViews('Cart.Lines.View', {
+                            'Item.Summary': {
+                                'JJ.test.test.Cart.View.Summary': {
+                                    childViewIndex: 10,
+                                    childViewConstructor: function () {
+                                        return new TestCartView({ model: this.model });
+                                    }
+                                }
+                            },
+                            'Item.Sku': {
+                                'JJ.test.test.Cart.View.Sku': {
+                                    childViewIndex: 11,
+                                    childViewConstructor: function () {
+                                        return new TestCartView({ model: this.model });
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
             }
         };
     }
 );
-
--- Workspace/test/Modules/test/JavaScript/test.View.js
+++ Workspace/test/Modules/test/JavaScript/test.View.js
@@ -29,13 +29,16 @@ define('JJ.test.test.View'
             if (this.pdp) {
                 this.pdp.on('afterOptionSelection', function() {
                     self.render();
+                    self.setOptions();
                 });
+                // Initial set options
+                this.setOptions();
             }
         },
         events: {
             'click [data-action="show-warranty-info"]': 'showWarrantyInfo',
             'change [data-action="update-delivery-note"]': 'validateDeliveryNote',
-            'blur [data-action="update-delivery-note"]': 'blurDeliveryNote'
+            'blur [data-action="update-delivery-note"]': 'setOptions'
         },
 
         showWarrantyInfo: function (e) {
@@ -50,18 +53,40 @@ define('JJ.test.test.View'
             }
         },
 
-        blurDeliveryNote: function (e) {
-            console.log('Delivery note input blurred.', $(e.currentTarget).val());
+        setOptions: function () {
+            if (this.isSettingOptions || !this.pdp) {
+                return;
+            }
+            this.isSettingOptions = true;
+            
+            try {
+                var deliveryNote = this.$('[data-action="update-delivery-note"]').val();
+                var itemInfo = this.pdp.getItemInfo();
+                var item = (itemInfo && itemInfo.item) ? itemInfo.item : {};
+
+                var expectedDelivery = (item.get ? item.get('custitem_jj_custitemexpecteddelivery') : item.custitem_jj_custitemexpecteddelivery) || '3-5 Business Days';
+                var specialNote = (item.get ? item.get('custitem_jj_custcolspecialnote') : item.custitem_jj_custcolspecialnote) || 'No special notes available.';
+
+                this.pdp.setOption('custitem_jj_custitemexpecteddelivery', expectedDelivery);
+                this.pdp.setOption('custitem_jj_custcolspecialnote', specialNote);
+                
+                if (deliveryNote) {
+                    this.pdp.setOption('custcol_jj_delivery_note', deliveryNote);
+                }
+            } catch (e) {
+                console.error('Error setting delivery options:', e);
+            } finally {
+                this.isSettingOptions = false;
+            }
         },
 
         getContext: function () {
             
             var itemInfo = this.pdp ? this.pdp.getItemInfo() : {};
             var item = itemInfo.item || {};
-
             return {
-                custitem_expected_delivery: item.custitem_jj_custitemexpecteddelivery || '3-5 Business Days',
-                custcol_special_note: item.custitem_jj_custcolspecialnote || 'No special notes available.',
+                custitem_expected_delivery: (item.get ? item.get('custitem_jj_custitemexpecteddelivery') : item.custitem_jj_custitemexpecteddelivery) || '3-5 Business Days',
+                custcol_special_note: (item.get ? item.get('custitem_jj_custcolspecialnote') : item.custitem_jj_custcolspecialnote) || 'No special notes available.',
                 showWarrantyInfo: true,
                 showDeliverySection: true
             };
